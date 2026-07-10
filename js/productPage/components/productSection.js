71f7ed8c74821195baa948c74e4405189616eea5
Naman Chauhan <namansinghchauhan6376@gmail.com>
2026-07-10 19:56:03 +0530

    Update

--- js/productPage/components/productSection.js
+++ js/productPage/components/productSection.js
@@ -10,9 +10,12 @@ function productSection() {
 
     let productHeading = document.querySelector(".prodHead h1")
     productHeading.innerText = product.name
-    let verticalCard = document.querySelector(".verCard")
-    verticalCard.addEventListener("click", ()=>{
-        verticalCard.style.border = "1px solid black" 
+    let verticalCard = document.querySelectorAll(".verCard")
+    verticalCard.forEach(card =>{
+        card.addEventListener("click", ()=>{
+            verticalCard.forEach(c=> c.classList.remove("active"))
+            card.classList.add("active")
+        })
     })
     let frontView = document.querySelector(".frontView img")
     frontView.src = product.frontImg
@@ -36,6 +39,26 @@ function productSection() {
     personView.src = product.personImg
     personView.addEventListener("click", () => {
         card.src = product.personImg
+    })
+
+    const colors = document.querySelectorAll(".color")
+    colors.forEach(color =>{
+        color.addEventListener("click",()=>{
+            colors.forEach(item=>{
+                item.classList.remove("active")
+            })
+            color.classList.add("active")
+        })
+    })
+
+    const choose = document.querySelectorAll(".size")
+    choose.forEach(size=>{
+        size.addEventListener("click", ()=>{
+            choose.forEach(item=>{
+                item.classList.remove("active")
+            })
+            size.classList.add("active")
+        })
     })
 
 }
